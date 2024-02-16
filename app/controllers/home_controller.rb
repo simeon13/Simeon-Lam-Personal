95efc582337a46ccf4c9c7669a69f6d9a6be8e10
Simeon Lam <simeon.j.lam@gmail.com>
2024-02-15 18:48:41 -0800

    set up other pages for navigation from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def career
+  end
+
+  def connect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/about'
+  get 'home/career'
+  get 'home/connect'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
