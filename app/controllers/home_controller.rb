3339f59a11dfb7b09f9018480c1b00d2ce40b7ea
Simeon Lam <simeon.j.lam@gmail.com>
2024-02-19 15:39:13 -0800

    Initial changes to connect page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,4 +10,12 @@ class HomeController < ApplicationController
 
   def connect
   end
+
+  def download_pdf
+    send_file(
+      "#{Rails.root}/app/assets/images/resume.pdf",
+      filename: "Simeon_Lam_Resume.pdf",
+      type: "application/pdf"
+    )
+  end
 end
