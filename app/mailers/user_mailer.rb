0c9cafa1a15785df3af7bbcb5296c8a11670f1a4
Simeon Lam <simeon.j.lam@gmail.com>
2024-02-19 21:27:53 -0800

    fix user mailer error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,10 @@ class UserMailer < ApplicationMailer
   include SendGrid
 
   def send_email(params)
-    from = Email.new(email: params[:contact_form][:email])
+    from = Email.new(email: "simeon.j.lam@gmail.com")
     to = Email.new(email: 'simeon.lam.dev@gmail.com')
-    subject = params[:contact_form][:subject]
-    content = Content.new(type: 'text/plain', value: 'params[:contact_form][:message]')
+    subject = "#{params[:contact_form][:name]} (#{params[:contact_form][:email]}): #{params[:contact_form][:subject]}"
+    content = Content.new(type: 'text/plain', value: params[:contact_form][:message])
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
