cd7c2363e4e33ad0304e72516863a089e144944e
Simeon Lam <simeon.j.lam@gmail.com>
2024-02-19 21:16:51 -0800

    testing mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  require 'sendgrid-ruby'
-  include SendGrid
-
   default from: "simeon.j.lam@gmail.com"
   layout "mailer"
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,7 @@
 class UserMailer < ApplicationMailer
+  require 'sendgrid-ruby'
+  include SendGrid
+
   def send_email(params)
     from = Email.new(email: params[:contact_form][:email])
     to = Email.new(email: 'simeon.lam.dev@gmail.com')
@@ -7,5 +10,6 @@ class UserMailer < ApplicationMailer
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
+    print response
   end
 end
