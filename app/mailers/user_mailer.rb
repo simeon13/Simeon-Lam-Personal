c3962e4d1f43eb70ce18bfb34a32525ce1abd3bb
Simeon Lam <simeon.j.lam@gmail.com>
2024-02-19 21:20:20 -0800

    Test headers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,8 @@ class UserMailer < ApplicationMailer
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    print response
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
